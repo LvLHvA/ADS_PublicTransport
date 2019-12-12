92d4926fdef4ea340c4be89169ca683cd121f2cf
Koen Lippe <koen.lippe@quicknet.nl>
2019-12-12 13:05:18 +0100

    Implemented DepthFirst Search

--- src/main/java/graphalgorithms/DepthFirstSearch.java
+++ src/main/java/graphalgorithms/DepthFirstSearch.java
@@ -1,4 +1,30 @@
 package graphalgorithms;
 
-public class DepthFirstSearch {
+import model.TransportGraph;
+
+public class DepthFirstSearch extends AbstractPathSearch {
+
+    public DepthFirstSearch(TransportGraph graph, String start, String end) {
+        super(graph, start, end);
+    }
+
+    @Override
+    public void search() {
+        nodesVisited.add(graph.getStation(startIndex)); //Add starting index to visited
+        searchRecursive(startIndex);
+        pathTo(endIndex);
+    }
+
+    public void searchRecursive(int v) {
+        marked[v] = true;
+        for (int adjacentVertex : graph.getAdjacentVertices(v)) {
+
+            if(!marked[adjacentVertex]) {
+                nodesVisited.add(graph.getStation(adjacentVertex));
+                edgeTo[adjacentVertex] = v;
+                searchRecursive(adjacentVertex);
+            }
+        }
+
+    }
 }
